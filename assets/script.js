7373fc9ccb4405cc2b2d89a2374cedda62686913
Erik  Enoka Aku <Erik@Jennifers-iMac.lan>
2023-11-03 14:15:06 -0700

    clean up code and add comments

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,30 @@
 var timeDisplayEl = $("#currentDay");
 var textareaEl = $("textarea");
 
+//Used to save key pairs in local storage
 var tasks = [];
 
 $(function () {
+  //renders local storage on page load
   init();
 
+  //Displays current day and time in header
   function displayTime() {
     var rightNow = dayjs().format("dddd, MMMM D");
     timeDisplayEl.text(rightNow);
   }
+
   displayTime();
 
+  //Select all textareas, compare id of textarea to current hour and add class based on comparison
   function changeColor() {
-    // select all textareas, compare id of textarea to current hour  and add class based on comparison
-
     // gets current hour
     var currentHour = dayjs().hour();
 
     textareaEl.each(function () {
       var scheduledTime = parseInt($(this).attr("id"));
 
+      //Color change base on current hour
       if (scheduledTime === currentHour) {
         $(this).addClass("present");
       } else if (scheduledTime < currentHour) {
@@ -33,27 +37,27 @@ $(function () {
 
   changeColor();
 
+  //When save button is clicked, grab id from parent div and use as key in Local storage, take user input and save to local storage
   $(".saveBtn").click(function (e) {
-    // when save button is clicked, grab id from parent div and use as key in Local storage, take user input and save to local storage
     e.preventDefault();
 
     var userInput = $(this).siblings(".description").val();
-    // console.log(userInput)
-
     var key = $(this).parent().attr("id");
-    // console.log(key)
 
     tasks.push({ key, userInput });
 
+    //Save to local storage
     localStorage.setItem("event", JSON.stringify(tasks));
   });
 
+  //renders calender events on page load from local storage
   function init() {
     storedTasks = JSON.parse(localStorage.getItem("event"));
 
     if (storedTasks !== null) {
       tasks = storedTasks;
 
+      //Loops through local storage, checks hour-x id of each object and displays the appropriate text to each calender section
       for (var i = 0; i < tasks.length; i++) {
         if (tasks[i].key === "hour-9") {
           $("#hour-9 .description").text(tasks[i].userInput);
@@ -85,56 +89,4 @@ $(function () {
       }
     }
   }
-});
-
-// *** not working
-// function init() {
-//   storedTasks = JSON.parse(localStorage.getItem('event'))
-// if (storedTasks !== null) {
-//   tasks = storedTasks
-
-// }
-
-// console.log(tasks)
-
-// }
-
-// ***** good one
-
-// $('.saveBtn').click(function(){
-//   // when save button is clicked, grab id from parent div and use as key in Local storage, take user input and save to local storage
-
-//   var key = $(this).parent().attr('id')
-//   console.log(key)
-
-//   var userInput = $(this).siblings('.input').val();
-
-//   localStorage.setItem(key, userInput)
-// })
-
-// $("#hour-9 .input").val(localStorage.getItem("hour-9"));
-// $("#hour-10 .input").val(localStorage.getItem("hour-10"));
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
